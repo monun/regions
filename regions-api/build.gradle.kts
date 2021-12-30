cc007cb8006418bd65aedd2d148d9cd2c30b625d
Monun <monun1010@gmail.com>
2021-12-31 04:54:47 +0900

    :rocket: 배포준비

---
+++ regions-api/build.gradle.kts
@@ -0,0 +1,92 @@
+plugins {
+    id("org.jetbrains.dokka") version "1.6.0"
+    `maven-publish`
+    signing
+}
+
+tasks {
+    create<Jar>("sourcesJar") {
+        archiveClassifier.set("sources")
+        from(sourceSets["main"].allSource)
+    }
+
+    create<Jar>("dokkaJar") {
+        archiveClassifier.set("javadoc")
+        dependsOn("dokkaHtml")
+
+        from("$buildDir/dokka/html/") {
+            include("**")
+        }
+    }
+}
+
+publishing {
+    repositories {
+        mavenLocal()
+        maven {
+            name = "central"
+
+            credentials.runCatching {
+                val nexusUsername: String by project
+                val nexusPassword: String by project
+                username = nexusUsername
+                password = nexusPassword
+            }.onFailure {
+                logger.warn("Failed to load nexus credentials, Check the gradle.properties")
+            }
+
+            url = uri(
+                if ("SNAPSHOT" in version as String) {
+                    "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+                } else {
+                    "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                }
+            )
+        }
+    }
+
+    publications {
+        create<MavenPublication>("regions-api") {
+            artifactId = "regions-api"
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["dokkaJar"])
+
+            pom {
+                name.set("regions-api")
+                description.set("Regions plugin api")
+                url.set("https://github.com/monun/regions")
+
+                licenses {
+                    license {
+                        name.set("GNU General Public License version 3")
+                        url.set("https://opensource.org/licenses/GPL-3.0")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("monun")
+                        name.set("Monun")
+                        email.set("monun1010@gmail.com")
+                        url.set("https://github.com/monun")
+                        roles.addAll("developer")
+                        timezone.set("Asia/Seoul")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/monun/regions.git")
+                    developerConnection.set("scm:git:ssh://github.com:monun/regions.git")
+                    url.set("https://github.com/monun/regions")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    isRequired = true
+    sign(tasks.jar.get(), tasks["sourcesJar"], tasks["dokkaJar"])
+    sign(publishing.publications["regions-api"])
+}
